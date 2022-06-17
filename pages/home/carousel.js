a741f16b17ab2e7df578c77901ba919990fb9e49
SwiftCoderJoe <coderjoe05@gmail.com>
2022-06-16 20:34:14 -0400

    new changed switcher

--- pages/home/carousel.js
+++ pages/home/carousel.js
@@ -1,5 +1,8 @@
 var currentSlide = 1
 
+let currentText = 0;
+let texts = ["iOS Developer", "Web Designer", "Frontend Engineer", "JavaScript Programmer", "Computer Technician", "Backend Engineer", "Web Developer", "SysAdmin", "Swift Programmer", ""]
+
 function switchView(viewNumber) {
     var projectCarousel = document.getElementById(`projectCarousel`)
 
@@ -8,3 +11,22 @@ function switchView(viewNumber) {
 
     currentSlide = viewNumber
 }
+
+window.addEventListener("load", () => {
+
+    let switchingText = document.getElementById(`mainSwitch`)
+
+    console.log(switchingText)
+    
+    setInterval(updateSwitch, 2000)
+
+    function updateSwitch() {
+        switchingText.classList.add(`invisible`)
+        setTimeout(() => {
+            switchingText.innerText = texts[currentText++ % texts.length]
+            switchingText.classList.remove(`invisible`)
+        }, 300)
+        
+    }
+
+})
