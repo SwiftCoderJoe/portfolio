0427ad9885512bb067a5a3cdb165fa65c35997ac
SwiftCoderJoe <coderjoe05@gmail.com>
2022-06-16 21:59:08 -0400

    Carousel buttons

--- pages/home/carousel.js
+++ pages/home/carousel.js
@@ -1,9 +1,15 @@
-var currentSlide = 1
+let currentSlide = 1
 
 let currentText = 0;
-let texts = ["iOS Developer", "Web Designer", "Frontend Engineer", "JavaScript Programmer", "Computer Technician", "Backend Engineer", "Web Developer", "SysAdmin", "Swift Programmer", ""]
+let texts = ["iOS Developer", "Web Designer", "Frontend Engineer", "JavaScript Programmer", "Computer Technician", "Backend Engineer", "Web Developer", "SysAdmin", "Swift Programmer", "Content Creator"]
+
+function negativeModFix(n, m) {
+    return ((n % m) + m) % m;
+}
 
 function switchView(viewNumber) {
+    console.log(viewNumber)
+
     var projectCarousel = document.getElementById(`projectCarousel`)
 
     projectCarousel.classList.remove(`slideTo${currentSlide}`)
@@ -15,8 +21,6 @@ function switchView(viewNumber) {
 window.addEventListener("load", () => {
 
     let switchingText = document.getElementById(`mainSwitch`)
-
-    console.log(switchingText)
     
     setInterval(updateSwitch, 2000)
 
