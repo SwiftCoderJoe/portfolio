46dd18a7f6c430c615c658e781edd5fe520acf21
Joe Cardenas <coderjoe05@gmail.com>
2023-01-09 21:03:50 -0500

    Modal dialogs for more projects

---
+++ src/projects/openDialog.js
@@ -0,0 +1,7 @@
+function openModal(dialog) {
+    let dialogElement = document.getElementById(dialog).showModal()
+}
+
+function closeModal(dialog) {
+    let dialogElement = document.getElementById(dialog).close()
+}
